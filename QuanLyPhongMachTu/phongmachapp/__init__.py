5087d76cbcb55680d44686a9ea40ae9cb64da57c
Nguyễn Trung Hậu <138142163+Hauharu@users.noreply.github.com>
2024-11-30 00:52:21 +0700

    Delete QuanLyPhongMachTu directory

--- QuanLyPhongMachTu/phongmachapp/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from urllib.parse import quote
-app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@quanlyphongmachtudb/testdb?charset=utf8mb4" % quote('Admin@123')
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["SECRET_KEY"]="PhongDoLaNhatThoi_DangCapLaMaiMai"
-db = SQLAlchemy(app)
